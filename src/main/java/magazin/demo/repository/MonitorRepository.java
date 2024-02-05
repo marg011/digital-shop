ea745191c4fc383a0f1dfb754526883c032cf2ce
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-02-05 18:37:12 +0300

    HardDiskController

--- src/main/java/magazin/demo/repository/MonitorRepository.java
+++ src/main/java/magazin/demo/repository/MonitorRepository.java
@@ -3,8 +3,6 @@ package magazin.demo.repository;
 import magazin.demo.entity.MonitorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MonitorRepository extends JpaRepository<MonitorEntity, Integer> {
 
 }
