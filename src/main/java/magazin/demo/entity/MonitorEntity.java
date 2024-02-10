b612708ce794c11320658728ae0cc37cb7c93593
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-02-11 01:01:32 +0300

    new MonitorEntity

--- src/main/java/magazin/demo/entity/MonitorEntity.java
+++ src/main/java/magazin/demo/entity/MonitorEntity.java
@@ -9,29 +9,17 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "мониторы")
 @Data
 public class MonitorEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
 
-    @Column(name = "серийный_номер")
     private long serialNumber;
-
-    @Column(name = "производитель")
     private String manufacturer;
-
-    @Column(name = "цена")
     private int price;
-
-    @Column(name = "количество")
     private int quantity;
-
-    @Column(name = "диагональ")
     private int diagonal;
-
 
 }
