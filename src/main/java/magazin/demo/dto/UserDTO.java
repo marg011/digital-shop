5cb336496f1cbd07b5102db05a8bf2ba0b01893b
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-02-14 02:08:21 +0300

    UserDTO+Role

---
+++ src/main/java/magazin/demo/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package magazin.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private int id;
+    private String name;
+    private Role role;
+    private String image;
+
+}
